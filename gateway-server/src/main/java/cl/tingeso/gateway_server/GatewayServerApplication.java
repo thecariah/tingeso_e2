25c70a3b42f91d08977247f030589aa275a67a0e
Carolina Antillanca <carolina.antillanca@usach.cl>
2024-06-09 02:05:03 -0400

    error registro en eureka 9

--- gateway-server/src/main/java/cl/tingeso/gateway_server/GatewayServerApplication.java
+++ gateway-server/src/main/java/cl/tingeso/gateway_server/GatewayServerApplication.java
@@ -3,10 +3,8 @@ package cl.tingeso.gateway_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
-@EnableEurekaClient
 public class GatewayServerApplication {
 
 	public static void main(String[] args) {
