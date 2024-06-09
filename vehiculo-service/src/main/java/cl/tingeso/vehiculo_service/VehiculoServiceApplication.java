fa8d01540ecfcf8f98c0b3b9675cf98ef14dcf96
Carolina Antillanca <carolina.antillanca@usach.cl>
2024-06-09 01:44:13 -0400

    error registro en eureka 7

--- vehiculo-service/src/main/java/cl/tingeso/vehiculo_service/VehiculoServiceApplication.java
+++ vehiculo-service/src/main/java/cl/tingeso/vehiculo_service/VehiculoServiceApplication.java
@@ -3,12 +3,10 @@ package cl.tingeso.vehiculo_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class},
 		scanBasePackages={"cl.tingeso.vehiculo_service.repositories"})
-@EnableEurekaClient
 public class VehiculoServiceApplication {
 
 	public static void main(String[] args) {
