c8892a2d66a96464d6cfc564446de04db0af39c5
Carolina Antillanca <carolina.antillanca@usach.cl>
2024-06-08 23:17:51 -0400

    intentando solucionar error 4

--- vehiculo-service/src/main/java/cl/tingeso/vehiculo_service/VehiculoServiceApplication.java
+++ vehiculo-service/src/main/java/cl/tingeso/vehiculo_service/VehiculoServiceApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class},
+		scanBasePackages={"cl.tingeso.vehiculo_service.repositories"})
 @EnableEurekaClient
 public class VehiculoServiceApplication {
 
