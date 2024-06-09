c7c417deb862458ebae37ce17313aeeef8e61671
Carolina Antillanca <carolina.antillanca@usach.cl>
2024-06-09 02:29:30 -0400

    arreglo dependencias microservicios

--- autofix-service/src/main/java/cl/tingeso/autofix_service/AutofixServiceApplication.java
+++ autofix-service/src/main/java/cl/tingeso/autofix_service/AutofixServiceApplication.java
@@ -3,10 +3,8 @@ package cl.tingeso.autofix_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
-@EnableEurekaClient
 public class AutofixServiceApplication {
 
 	public static void main(String[] args) {
--- historial-service/src/main/java/cl/tingeso/historial_service/HistorialServiceApplication.java
+++ historial-service/src/main/java/cl/tingeso/historial_service/HistorialServiceApplication.java
@@ -3,11 +3,9 @@ package cl.tingeso.historial_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class},
 		scanBasePackages={"cl.tingeso.historial_service.repositories"})
-@EnableEurekaClient
 public class HistorialServiceApplication {
 
 	public static void main(String[] args) {
