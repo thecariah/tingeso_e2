42749f2c7ace5d7f21d2109341ea98560628b7cc
Carolina Antillanca <carolina.antillanca@usach.cl>
2024-06-08 21:36:55 -0400

    detalle config-server
    Error: Failed to configure a DataSource: 'url' attribute is not specified and no embedded datasource could be configured.

--- config-server/src/main/java/cl/tingeso/config_server/ConfigServerApplication.java
+++ config-server/src/main/java/cl/tingeso/config_server/ConfigServerApplication.java
@@ -2,9 +2,10 @@ package cl.tingeso.config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @EnableConfigServer
 public class ConfigServerApplication {
 
